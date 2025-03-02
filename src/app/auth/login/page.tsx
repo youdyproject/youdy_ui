c3d301f8809c9a9cf1f72fb941d86a5968cc7c6a
미녕 <als543413@gmail.com>
2025-03-02 21:27:46 +0900

    feat: 로그인페이지css개발중

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -3,10 +3,20 @@ import { Button } from "@/components/ui/Button"
 
 export default function Page() {
     return (
-        <div className="flex flex-col gap-4 p-4">
-            <label className="text-sm font-medium">아이디</label>
-            <Input type="text" placeholder="아이디" />
-            <Button>제출</Button>
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <div className="w-[400px] border rounded-lg">
+                <Input
+                type="text"
+                placeholder="아이디"
+                className="w-full border-none rounded-none px-4 py-2 focus:ring-2"
+                />
+                <Input
+                type="password"
+                placeholder="비밀번호"
+                className="w-full border-none rounded-none px-4 py-2"
+                />
+            </div>
+            <Button className="mt-4 w-[400px]">로그인</Button>
         </div>
     );
 }
